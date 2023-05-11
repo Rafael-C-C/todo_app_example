1fd270fed8473e0db1f3dcc8d31125a8965303f0
PAULINA-MAY <feripau230@gmail.com>
2023-05-11 09:17:33 -0600

    subiendo boton reuzable,row buttons y el login

---
+++ lib/core/widgets/buttonP/reusable_elevated_buttonP.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ReusableElevatedButtonP extends StatelessWidget {
+    final void Function()? function;
+    final String title;
+    final ButtonStyle? styledButton;
+
+  const ReusableElevatedButtonP({super.key,  required this.title, this.styledButton, this.function});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed:  () => function,
+      style: styledButton,
+      child: Text(title),
+    );
+  }
+}
---
+++ lib/features/login/screens/login_screenP.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:todo_app_tus_test/core/widgets/buttons/reusable_elevated_button.dart';
+import 'package:todo_app_tus_test/core/widgets/forms/reusable_form_field.dart';
+import 'package:todo_app_tus_test/features/home/screens/home_screen.dart';
+
+class LoginScreenP extends StatefulWidget {
+  const LoginScreenP({super.key});
+
+  @override
+  State<LoginScreenP> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreenP> {
+  final _loginKey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  String _email = '';
+  String _password = '';
+  bool hidepass = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              const FlutterLogo(size: 100),
+              const Text("Inicio de sesión"),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Form(
+                  key: _loginKey,
+                  child: Column(
+                    children: [
+                      CustomFormField(
+                          textController: emailController,
+                          labelText: 'Usuario:',
+                          hintText: 'Ejemplo@gmail.com',
+                          maxLiness: 1,
+                          prefixIcon: Icons.email,
+                          inputFormatters: [
+                            LengthLimitingTextInputFormatter(40)
+                          ],
+                          validator: (String? value) {
+                            if (value!.isEmpty) {
+                              return 'Este campo no puede estar vacío';
+                            }
+                            if (!RegExp(
+                                    "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+                                .hasMatch(value)) {
+                              return 'Por favor ingrese un correo valido.';
+                            }
+                            return null;
+                          },
+                          onSaved: (String? emaiLa) {
+                            _email = emaiLa!;
+                          }),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      CustomFormField(
+                        textController: passwordController,
+                        labelText: 'Contraseña:',
+                        maxLiness: 1,
+                        obscureText: hidepass,
+                        inputFormatters: [LengthLimitingTextInputFormatter(40)],
+                        prefixIcon: Icons.lock,
+                        suffix: InkWell(
+                          onTap: _togglepass,
+                          child: Icon(
+                            hidepass ? Icons.visibility : Icons.visibility_off,
+                            color: Colors.blue,
+                          ),
+                        ),
+                        validator: (String? value) {
+                          if (value!.isEmpty) {
+                            return 'Este campo no puede estar vacío';
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              ReusableElevatedButton(
+                title: 'Iniciar sesión',
+                press: () {
+                  if (_loginKey.currentState!.validate()) {
+                    _email = emailController.text;
+                    _password = passwordController.text;
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const HomeScreen()));
+                  } else {
+                    ('Por favor llene los campos correctamente');
+                  }
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _togglepass() {
+    setState(() {
+      hidepass = !hidepass;
+    });
+  }
+}
